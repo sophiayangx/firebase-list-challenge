48361af497f96fabfad8c1fb0fee385c396e6992
sophiayangx <sopyang@nuevaschool.org>
2016-10-05 08:43:50 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 var input = $("#new-item-input")
+var clearButton = $("#clear-button")
 
 input.keydown(function(e) {	
   if (e.keyCode == "13") {
     var valueToSave = input.val();
-    addListItem(valueToSave);
+    addListItem("sophiasList", valueToSave);
   }
+})
+
+clearButton.click(function(e) {	
+  deleteValue("sophiasList", valueToSave)
 })
 
 onNewListItem("sophiasList", function(value) {
