bb61141e1d4903ad68e4fc97fd8127365b3c1943
sophiayangx <sopyang@nuevaschool.org>
2016-10-05 08:33:42 -0700

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,12 @@
+var input = $("")
+
+input.keydown(function(e) {	
+  if (e.keyCode == "13") {
+    var valueToSave = input.val();
+    addListItem(valueToSave);
+  }
+})
+
+onNewListItem("davidsList", function(value) {
+  $("#list-display").append("<div>" + value + "</div>")
+}) 
